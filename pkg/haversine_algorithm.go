af2a8b16746f1f380ab77c30eed9af4890d33cd8
Paulo Abreu <pa.aabreu18@gmail.com>
2025-03-04 00:07:05 -0300

    feat: create haversine algorithm

---
+++ pkg/haversine_algorithm.go
@@ -0,0 +1,16 @@
+package pkg
+
+import "math"
+
+func HaversineDistance(lat1, lon1, lat2, lon2 float64) float64 {
+	const earthRadius = 6371000 // meters
+	dLat := (lat2 - lat1) * (math.Pi / 180.0)
+	dLon := (lon2 - lon1) * (math.Pi / 180.0)
+
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(lat1*(math.Pi/180.0))*math.Cos(lat2*(math.Pi/180.0))*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	return earthRadius * c
+}
