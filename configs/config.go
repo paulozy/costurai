42d52beec490ae065c43086f78b277377c2c430f
paulozy <dev.pa.aabreu18@gmail.com>
2025-05-17 13:54:55 -0300

    chore: add new packages and env variables

--- configs/config.go
+++ configs/config.go
@@ -3,19 +3,24 @@ package configs
 import "github.com/spf13/viper"
 
 type config struct {
-	DBDriver          string `mapstructure:"DB_DRIVER"`
-	DBHost            string `mapstructure:"DB_HOST"`
-	DBPort            string `mapstructure:"DB_PORT"`
-	DBUser            string `mapstructure:"DB_USER"`
-	DBPassword        string `mapstructure:"DB_PASSWORD"`
-	DBName            string `mapstructure:"DB_NAME"`
-	WebPort           string `mapstructure:"WEB_PORT"`
-	WebHost           string `mapstructure:"WEB_HOST"`
-	JWTSecret         string `mapstructure:"JWT_SECRET"`
-	JWTExpiresIn      int64  `mapstructure:"JWT_EXPIRES_IN"`
-	FirebaseProjectId string `mapstructure:"FIREBASE_PROJECT_ID"`
-	DBType            string `mapstructure:"DB_TYPE"`
-	Env               string `mapstructure:"ENV"`
+	DBDriver            string `mapstructure:"DB_DRIVER"`
+	DBHost              string `mapstructure:"DB_HOST"`
+	DBPort              string `mapstructure:"DB_PORT"`
+	DBUser              string `mapstructure:"DB_USER"`
+	DBPassword          string `mapstructure:"DB_PASSWORD"`
+	DBName              string `mapstructure:"DB_NAME"`
+	WebPort             string `mapstructure:"WEB_PORT"`
+	WebHost             string `mapstructure:"WEB_HOST"`
+	JWTSecret           string `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn        int64  `mapstructure:"JWT_EXPIRES_IN"`
+	FirebaseProjectId   string `mapstructure:"FIREBASE_PROJECT_ID"`
+	TwilioSID           string `mapstructure:"TWILIO_ACCOUNT_SID"`
+	TwilioAuthToken     string `mapstructure:"TWILIO_AUTH_TOKEN"`
+	TwilioSMSServiceSID string `mapstructure:"TWILIO_SMS_SERVICE_SID"`
+	SMSTimeout          int64  `mapstructure:"SMS_TIMEOUT"`
+	TwilioChannel       string `mapstructure:"TWILIO_CHANNEL"`
+	DBType              string `mapstructure:"DB_TYPE"`
+	Env                 string `mapstructure:"ENV"`
 }
 
 func LoadConfig(path string) (*config, error) {
--- go.mod
+++ go.mod
@@ -6,9 +6,10 @@ require (
 	cloud.google.com/go/firestore v1.18.0
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.9.1
-	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/google/uuid v1.6.0
 	github.com/spf13/viper v1.18.2
+	github.com/twilio/twilio-go v1.26.1
 	golang.org/x/crypto v0.31.0
 	google.golang.org/api v0.214.0
 )
@@ -33,6 +34,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
@@ -46,6 +48,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
